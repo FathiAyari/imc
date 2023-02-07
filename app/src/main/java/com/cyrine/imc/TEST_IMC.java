32f594160ed836c87f8964b0c608b986e6f0ee74
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2023-02-07 02:33:37 +0100

    first commit

---
+++ app/src/main/java/com/cyrine/imc/TEST_IMC.java
@@ -0,0 +1,14 @@
+package com.cyrine.imc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TEST_IMC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_imc);
+    }
+}
